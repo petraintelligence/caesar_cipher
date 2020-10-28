57c44665517f0c49b43af225abd6b7ee431daeea
PetraIntelligence <brown.joshua.scott@gmail.com>
2020-10-28 10:08:39 -0500

    Basic comments for program steps

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 First Ruby project with the objective of creating a caesar cipher.
+
+Requirements
+- convert strings to numbers
+- wrap from z to a
+- keep the same case
---
+++ cipher.rb
@@ -0,0 +1,13 @@
+# Take in a string from input
+
+
+# Store the string in an array
+
+
+# iterate through the array for each letter and convert to number
+
+
+# add the number of shifts
+
+
+# return the encrypted string
