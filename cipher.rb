c8999d1546085a70ef59d2e6c08c6e43618e6098
PetraIntelligence <brown.joshua.scott@gmail.com>
2020-10-28 11:41:01 -0500

    final project complete

--- cipher.rb
+++ cipher.rb
@@ -1,17 +1,25 @@
+def shift_character(a, base_number, shift_number)
+    (((a.ord - base_number + shift_number) % 26 + base_number)).chr
+end
+
+def caesar_cipher(string, shift_number)
+    string.chars.map do |a|
+        case a
+        when "a".."z"
+            shift_character(a, "a".ord, shift_number)
+        when "A".."Z"
+            shift_character(a, "A".ord, shift_number)
+        else
+            a 
+        end
+    end.join
+end
+
 puts "Input your word to cipher: "
 input_string = gets.chomp
 
 puts "Input your number to shift by: "
-shift_number = gets.chomp
+shift_number = gets.chomp.to_i
 
-# Store the string in an array
+p caesar_cipher(input_string, shift_number)
 
-
-
-# iterate through the array for each letter and convert to number
-
-
-# add the number of shifts
-
-
-# return the encrypted string
